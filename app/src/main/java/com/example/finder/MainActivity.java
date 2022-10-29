79c788261db74bf5c0d98ee98bef916566663afa
Brigith <brigith.a.r.q@gmail.com>
2022-10-29 03:50:59 -0300

    Version 3.0-tercer proyecto

--- app/src/main/java/com/example/finder/MainActivity.java
+++ app/src/main/java/com/example/finder/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.finder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    Button btnFind, btnFinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar= findViewById(R.id.pgsBar);
+        btnFind= findViewById(R.id.btnFind);
+        btnFinder= findViewById(R.id.btnFinder);
+
+        btnFind.setOnClickListener(view -> {
+            progressBar.setVisibility(View.VISIBLE);
+            new Hilo1().start();
+        });
+
+
+        btnFinder.setOnClickListener(view -> {
+
+            progressBar.setVisibility(View.VISIBLE);
+            new Hilo1().start();
+        });
+
     }
+
+    class Hilo1 extends Thread{
+
+        @Override
+
+        public void run(){
+            try {
+                Thread.sleep(10000);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Cargando", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
