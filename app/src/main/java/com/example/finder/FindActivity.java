73d173df1a75011b95c9cc01495d464147dbb830
Brigith <brigith.a.r.q@gmail.com>
2022-10-29 23:58:52 -0300

    Version 5.0-tercer proyecto

--- app/src/main/java/com/example/finder/FindActivity.java
+++ app/src/main/java/com/example/finder/FindActivity.java
@@ -2,13 +2,48 @@ package com.example.finder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class FindActivity extends AppCompatActivity {
+
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    int whip=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor==null)
+        {finish();}
+
+        sensorEventListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float x=sensorEvent.values[0];
+                System.out.println("valor giro"+x);
+                if(x<-5 && whip==0){
+                    whip++;
+                    Toast.makeText(FindActivity.this, "No muevas mucho el celular", Toast.LENGTH_SHORT).show();
+                }
+                else if(x>5 && whip==1){
+                    Toast.makeText(FindActivity.this, "No muevas mucho el celular", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
     }
 }
